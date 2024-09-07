c209c0a7fe6a4ddafeab4d1f7fb2956146bb9462
gauravdave083 <gd836398@gmail.com>
2024-09-07 19:33:59 +0530

    readme

---
+++ lesson.py
@@ -0,0 +1,16 @@
+# How to Approach :
+
+#       * 
+#       * *
+#       * * *
+#       * * * *   
+
+
+
+#      1. no. of lines = no of rows = no of times outer loop will run
+
+#      2. Identify for every row no., 
+    # @ how many columns are there ?
+    # @ Types of elements in colummn 
+
+#      3. What do you need to print ? 
